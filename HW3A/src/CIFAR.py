34ca0083763b352f41f1b05737395d58d1ff083d
Ellie Nash <ellienashmo@gmail.com>
2025-03-18 16:58:59 -0500

    Added 4 class networks

--- HW3A/src/CIFAR.py
+++ HW3A/src/CIFAR.py
@@ -22,6 +22,13 @@ transform = transforms.Compose(
 
 batch_size = 4
 
+'''DATA LOADERS FOR 4 CLASSES
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform, target_transform=lambda x : (0 if x == 1 else 1 if x == 9 else 2 if x == 8 else 3))
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform, target_transform=lambda x : (0 if x == 1 else 1 if x == 9 else 2 if x == 8 else 3))
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)'''
+
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
 
@@ -35,7 +42,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
+    npimg = torch.Tensor.cpu(img).numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
@@ -43,7 +50,7 @@ def imshow(img):
 ########################################################################
 # 2. Define a Convolutional Neural Network
 
-class Net(nn.Module):
+class Net1(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
@@ -62,11 +69,69 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
+class Net2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 48, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(48, 64, 5, padding=2)
+        self.fc1 = nn.Linear(64 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 10)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class Net3(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 4)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class Net4(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 48, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(48, 64, 5, padding=2)
+        self.fc1 = nn.Linear(64 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 4)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
 #Defines TPU device
 dev = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(dev)
 
-net = Net().to(dev)
+net = Net2().to(dev)
 
 ########################################################################
 # 3. Define a Loss function and optimizer
@@ -77,7 +142,7 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 ########################################################################
 # 4. Train the network
 
-'''start = timeit.default_timer()
+start = timeit.default_timer()
 
 for epoch in range(4):  # loop over the dataset multiple times
 
@@ -85,6 +150,7 @@ for epoch in range(4):  # loop over the dataset multiple times
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data[0].to(dev), data[1].to(dev)
+        #print(labels)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -106,24 +172,15 @@ stop = timeit.default_timer()
 print(f'Finished Training in {stop - start}')
 
 # Save trained model:
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)'''
+PATH = './cifar_net2.pth'
+torch.save(net.state_dict(), PATH)
 
 ########################################################################
 # 5. Test the network on the test data
 
-'''
-dataiter = iter(testloader)
-images, labels = next(dataiter)
-
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
-'''
-
-PATH = './cifar_net.pth'
-net = Net().to(dev)
-net.load_state_dict(torch.load(PATH, weights_only=True))
+'''PATH = './cifar_net2.pth'
+net = Net2().to(dev)
+net.load_state_dict(torch.load(PATH, weights_only=True))'''
 
 
 correct = 0
@@ -149,30 +206,11 @@ print(f'Accuracy of the network on the 10000 test images: {100 * correct // tota
 # Hmmm, what are the classes that performed well, and the classes that did
 # not perform well:
 
+catDogImages = []
+
 # prepare to count predictions for each class
 correct_pred = {classname: 0 for classname in classes}
 total_pred = {classname: 0 for classname in classes}
-
-# again no gradients needed
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data[0].to(dev), data[1].to(dev)
-        outputs = net(images)
-        _, predictions = torch.max(outputs, 1)
-        # collect the correct predictions for each class
-        for label, prediction in zip(labels, predictions):
-            if label == prediction:
-                correct_pred[classes[label]] += 1
-            total_pred[classes[label]] += 1
-
-
-# print accuracy for each class
-for classname, correct_count in correct_pred.items():
-    accuracy = 100 * float(correct_count) / total_pred[classname]
-    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
-
-
-
 
 # prepare to calculate confusion matrix
 conf_matrix = np.zeros((10,10), dtype=np.float32)
@@ -186,18 +224,25 @@ with torch.no_grad():
         outputs = net(images)
         _, predictions = torch.max(outputs, 1)
         # collect the correct predictions for each class
+        bIndex = 0
         for label, prediction in zip(labels, predictions):
+            if classes[label] == 'cat' and classes[prediction] == 'dog':
+                catDogImages.append(images[bIndex])
             if label == prediction:
                 correct_pred[classes[label]] += 1
             else:
                 conf_matrix[classes.index(classes[label])][classes.index(classes[prediction])] += 1
             total_pred[classes[label]] += 1
+            bIndex += 1
 
+imshow(torchvision.utils.make_grid(catDogImages[8:13]))
 
 # print accuracy for each class
 for classname, correct_count in correct_pred.items():
     accuracy = 100 * float(correct_count) / total_pred[classname]
     print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
     for classnameSUB in classes:
-        confusion = 100 * float()
-        print(f'Confusion percentage for {classname} is {conf_matrix[classes.index(classes[classname])][classes.index(classes[classnameSUB])]} %')
+        print(classname)
+
+        confusion = 100 * float(conf_matrix[classes.index(classname)][classes.index(classnameSUB)]) / total_pred[classname]
+        print(f'{classname:5s} x {classnameSUB:5s} = {confusion:.1f} %')
